b624087f1edf4d6551f317951e72bc0249a327f8
aliallam123 <bantalibt@Gmail.com>
2025-11-23 12:14:30 +0000

    fix pdf error

--- data_extraction.py
+++ data_extraction.py
@@ -5,7 +5,7 @@ import re
 
 def extract_text_from_pdf(pdf_file: str) -> [str]:
     with open(pdf_file, 'rb') as pdf:
-        reader = PyPDF2.PdfReader(pdf_file)
+        reader = PyPDF2.PdfReader(pdf)
         pdf_text = []
 
         for page in reader.pages:
